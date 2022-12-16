ea6159ec4ee2223137f0a97ef6f42922db3291b0
DhineshKannan97 <dhineshkannan.madhavan@gcitsolutions.com>
2022-12-16 19:34:48 +0530

    stage 1

--- src/main/java/com/socket/Server.java
+++ src/main/java/com/socket/Server.java
@@ -1,37 +1,23 @@
 package com.socket;
-
-import java.io.BufferedInputStream;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-
 public class Server {
-
     public void socServer(int port) throws IOException {
-        Socket socket;
         ServerSocket serverSocket;
-        DataInputStream input;
+        Socket socket;
+        DataInputStream input=null;
+        DataOutputStream output = null;
         serverSocket = new ServerSocket(port);
-
+        System.out.println("server start");
         while (true) {
-            System.out.println("server start");
-            System.out.println(serverSocket);
             socket = serverSocket.accept();
             System.out.println("Client accepted");
             System.out.println(socket);
-            try {
-                input = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
-                String str = "";
-                while (!str.equals("end")) {
-                    str = input.readUTF();
-                    System.out.println(str);
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-//            serverSocket.close();
-//            socket.close();
+            Thread thread = new ServerThread(socket,input,output);
+            thread.start();
         }
     }
 
